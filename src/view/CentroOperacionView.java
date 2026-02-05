ded50e30918979f505682bfaa12947ef0105d684
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-05 16:00:05 -0500

    commit - CentroOperacionView .

--- src/view/CentroOperacionView.java
+++ src/view/CentroOperacionView.java
@@ -1,4 +1,27 @@
 package view;
 
-public class CentroOperacionView {
+import javax.swing.*;
+
+public class CentroOperacionView extends JFrame {
+
+    public JTextField txtId = new JTextField(5);
+    public JTextField txtNombre = new JTextField(15);
+    public JButton btnGuardar = new JButton("Guardar");
+
+    public CentroOperacionView() {
+
+        setTitle("Centro Operaciones");
+        setLayout(new java.awt.FlowLayout());
+
+        add(new JLabel("ID"));
+        add(txtId);
+
+        add(new JLabel("Nombre"));
+        add(txtNombre);
+
+        add(btnGuardar);
+
+        setSize(400, 200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
 }

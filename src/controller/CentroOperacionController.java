4327a0ec750a9855aca2904a3e3fcb89ffee8063
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-05 12:10:16 -0500

    commit - CentroOperacionController .

--- src/controller/CentroOperacionController.java
+++ src/controller/CentroOperacionController.java
@@ -1,4 +1,26 @@
 package controller;
 
+import model.*;
+import view.*;
+
+import javax.swing.*;
+
 public class CentroOperacionController {
+
+    public CentroOperacionController(CentroOperacionView view) {
+
+     /*    view.btnGuardar.addActionListener(e -> {
+            try {
+                CentroOperacion c = new CentroOperacion();
+                c.setId(Integer.parseInt(view.txtId.getText()));
+                c.setNombre(view.txtNombre.getText());
+
+                new CentroOperacionDAO().ejecutarSP(1, c);
+
+                JOptionPane.showMessageDialog(view, "Registro guardado correctamente .");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        });  */
+    }
 }

b6b235366518c219da9420e0f413fb14ea3a65c5
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-05 11:38:34 -0500

    commit - CentroOperacion .

--- src/model/CentroOperacion.java
+++ src/model/CentroOperacion.java
@@ -1,4 +1,50 @@
 package model;
 
 public class CentroOperacion {
+
+    private int id;
+    private String nombre;
+    private String ubicacion;
+    private String responsable;
+    private String estado;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
